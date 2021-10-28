d43f42ed27762987ffddc03dc9e337c3610b52a6
giangpham1210 <75287260+giangpham1210@users.noreply.github.com>
2021-10-28 16:23:06 +0900

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -35,6 +35,9 @@ def clean_data(df):
     
         # convert column from string to numeric
         categories[column] = categories[column].astype(int)
+        
+        # convert values to binary
+        categories[column][categories[column] > 1] = 1
     
     # drop the original categories column from `df`
     df = df.drop(columns = "categories")
